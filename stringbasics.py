ad4b6c8134cef806c387217982283674e4b4c5db
mahadi24t <mahadi24t@outlook.com>
2025-04-20 23:40:51 +0600

    string_slice

---
+++ stringbasics.py
@@ -0,0 +1,23 @@
+msg='welcome to it\'s Python 101: Strings'
+print(msg)
+print(msg,msg)
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.title())
+
+print(len(msg))
+print(msg.count('o'))
+
+
+#slicing
+
+print(msg[:7])
+
+msg = "welcome ring to tyler"
+
+print(msg.title())
+# print(msg[:-34])
+
+print(msg[::-1])
+
